6da99d4ddb4366f031a9aafede0872d615b455a2
Alpha Keita <78216570+alphakta@users.noreply.github.com>
2021-12-13 12:12:49 +0100

    maj

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,7 @@
 import { createConnection } from "net";
 import { createInterface } from "readline";
 
-let currentCommand = '';
-
+let currentCommand = ''
 const client = createConnection({ port: process.argv[2], host: 'localhost' }, () => {
   console.log("Client connected.");
 });
@@ -15,46 +14,33 @@ client.on("data", (data) => {
 
   const message = data.toString();
   console.log("Message received:", message);
-
   const [status, ...args] = message.trim().split(" ");
-  
-  if (status == 331 || status == 530 && currentCommand == "USER") {
-
-    rl.on("line", (input) => {
-      let string = input.split(' ')
-      if (string[0] == 'PASS') {
-        client.write(input);
-        currentCommand = 'PASS'
-      }
-      else
-        console.log('--- Please enter a password ---')
-    });
-  }
+  console.log("taper commande :")
 
   if (status == 220) {
-    currentCommand = "USER";
 
     rl.on("line", (input) => {
       let string = input.split(' ')
 
-      if (string[0] == 'USER') 
+      if (string[0] == 'USER') {
         client.write(input);
-      else if (string[0] == 'HELP' || string[0] == 'QUIT')
+        currentCommand = 'USER'
+      }
+      else if (currentCommand == 'USER' && string[0] != 'PASS') {
+        console.log('Please enter a password !')
+      } else if (currentCommand == 'USER' && string[0] == 'PASS') {
+        client.write(input);
+        currentCommand = 'PASS'
+      } else if (currentCommand == 'PASS')
         client.write(input)
       else
-        console.log('Please log in')
+        console.log('Please log in', status)
     });
   }
 
-  if (status == 221) {
-    currentCommand = "QUIT"
+  if (status == 221)
     process.exit()
-  }
 
-  if (status == 230 || status == 257) {
-    rl.on("line", (input) => {
-      client.write(input);
-    });}
 });
 
 
